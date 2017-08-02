a4b5afb0e9824e806770abc2d57f6581986ce418
Benjamín Peña Olvera <benjamin.pena.olvera@gmail.com>
2017-08-01 20:34:47 -0500

    Products Controller Model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
+
+  private
+    def find_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :brand, :description, :price)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801171331) do
+ActiveRecord::Schema.define(version: 20170801233622) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.text     "description"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
